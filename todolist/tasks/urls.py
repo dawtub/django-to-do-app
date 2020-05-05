bdb37de48fbae6c7652f9caf3b1188da9bc37cc3
dawtub <tubisz.dawid@gmail.com>
2020-05-05 17:29:36 +0200

    removed unnecessary code and added completing the task

--- todolist/tasks/urls.py
+++ todolist/tasks/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'tasks'
 urlpatterns = [
     path('', views.TasksView.as_view(), name='index'),
-    path('add/', views.CreateTaskView.as_view(), name='add'),
-    path('delete/<int:pk>', views.DeleteTaskView.as_view(), name='delete'),
+    path('add/', views.TasksView.as_view(), name='add'),
+    path('<int:pk>/delete', views.DeleteTask.as_view(), name='delete'),
+    path('<int:task_id>/complete', views.complete, name='complete'),
 ]
--- todolist/tasks/views.py
+++ todolist/tasks/views.py
@@ -1,19 +1,10 @@
 from django.views import generic
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy
 
 from .models import Task
 from .forms import TaskForm
-
-
-class CreateTaskView(generic.CreateView):
-    form_class = TaskForm
-    template_name = 'tasks/new.html'
-    success_url = reverse_lazy('tasks:index')
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["tasks_list"] = Task.objects.all()
-        return context
 
 
 class TasksView(generic.CreateView):
@@ -28,9 +19,17 @@ class TasksView(generic.CreateView):
         return context
 
 
-class DeleteTaskView(generic.DeleteView):
+class DeleteTask(generic.DeleteView):
     model = Task
     success_url = reverse_lazy('tasks:index')
 
     def get(self, request, *args, **kwargs):
         return self.delete(request, *args, **kwargs)
+
+
+def complete(self, task_id):
+    task = get_object_or_404(Task, pk=task_id)
+    task.completed = True
+    task.save()
+    return HttpResponseRedirect(reverse_lazy('tasks:index'))
+
